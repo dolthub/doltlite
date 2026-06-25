5417ca9fcb9a55b1f1f271dc0a73cca47c59d000
Tim <tim@example.com>
2026-06-25 10:21:54 -0700

    Persist user_version in DoltLite catalogs

--- test/lib/doltlite_suite_manifest.sh
+++ test/lib/doltlite_suite_manifest.sh
@@ -67,6 +67,7 @@ doltlite_pragma_encoding.sh
 doltlite_pragma_journal_mode.sh
 doltlite_pragma_memory_journal_mode.sh
 doltlite_pragma_page_count.sh
+doltlite_pragma_user_version.sh
 doltlite_pragma_wal_checkpoint.sh
 doltlite_merge_ignore_corners.sh
 doltlite_record_format.sh
@@ -121,6 +122,7 @@ doltlite_pragma_auto_vacuum.sh
 doltlite_pragma_journal_mode.sh
 doltlite_pragma_memory_journal_mode.sh
 doltlite_pragma_page_count.sh
+doltlite_pragma_user_version.sh
 doltlite_pragma_wal_checkpoint.sh
 doltlite_row_count_estimate.sh
 doltlite_schema_cookie.sh
